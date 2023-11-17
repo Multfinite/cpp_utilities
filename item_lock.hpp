80c8046bd9de92b23284f67f9942c28021b20a56
Multfinite <Multfinite@gmail.com>
2023-11-17 04:27:40 +0300

    item lock fixes

--- item_lock.hpp
+++ item_lock.hpp
@@ -2,10 +2,14 @@
 #define UTILITIES_ITEM_LOCK_HPP
 
 #include <mutex>
+#include <functional>
 
 namespace Utilities
 {
 	using std::mutex;
+	using std::list;
+	using std::vector;
+	using std::reference_wrapper;
 
 	struct __try_lock {};
 	template<typename T>
@@ -15,7 +19,7 @@ namespace Utilities
 		
 	private:
 		T& _item;
-		vector<std::mutex&> _mutexes;
+		list<reference_wrapper<std::mutex>> _mutexes;
 	public:
 		T& item() { return _item; }
 		item_lock(T& item) : _item(item) {}
@@ -23,12 +27,12 @@ namespace Utilities
 		template<typename ...TMutexes>
 		item_lock(T& item, TMutexes&... mutexes) :  _item(item) 
 		{
-			lock(mutexes);
+			lock(mutexes...);
 		}
 		template<typename ...TMutexes>
 		item_lock(T& item, __try_lock ty, TMutexes&... mutexes) : _item(item)
 		{
-			auto index = try_lock(mutexes);
+			auto index = try_lock(mutexes...);
 			if (index != -1)
 				throw std::runtime_error{ std::to_string(index) };
 		}
@@ -38,22 +42,35 @@ namespace Utilities
 		}
 
 		template<typename ...TMutexes>
-		void lock(TMutexes&&... mutexes)
+		void lock(TMutexes&... mutexes)
 		{
-			std::lock(...mutexes);
-			_mutexes = { ...mutexes };
+			std::lock(mutexes);
+			_mutexes = { mutexes };
+		}
+		template<>
+		void lock<std::mutex>(std::mutex& mutex)
+		{
+			mutex.lock();
+			_mutexes = { mutex };
 		}
 		template<typename ...TMutexes>
-		auto try_lock(TMutexes&&... mutexes)
+		auto try_lock(TMutexes&... mutexes)
 		{
-			auto r = std::try_lock(...mutexes);
-			_mutexes = { ...mutexes };
+			auto r = std::try_lock(mutexes);
+			_mutexes = { mutexes };
+			return r;
+		}
+		template<>
+		auto try_lock<std::mutex>(std::mutex& mutex)
+		{
+			int r = mutex.try_lock() ? -1 : 0;
+			_mutexes = { mutex };
 			return r;
 		}
 		void unlock()
 		{
 			for (auto& m : _mutexes)
-				m.unlock();
+				m.get().unlock();
 		}
 	};
 }
