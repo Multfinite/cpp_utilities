03c23bc367b876e71e94093555b584fc0a27bbe5
Mikhail Kuznetcov <multfinite@gmail.com>
2024-05-31 15:48:15 +0300

    Update concurrent_list.hpp #2

--- concurrent_list.hpp
+++ concurrent_list.hpp
@@ -12,6 +12,7 @@ namespace Utilities {
     {
     public:
         using list_type = std::list<T>;
+        using list = concurrent_list<T>;
         using value_type = typename list_type::value_type;
         using allocator_type = typename list_type::allocator_type;
         using size_type = typename list_type::size_type;
@@ -59,23 +60,35 @@ namespace Utilities {
         const_iterator rend() const { return _l.rend(); }
 
         bool empty() const noexcept { return _l.empty(); }
-        size_t size() const noexcept { return _l.size(); }
-        size_t max_size() const noexcept { return _l.max_size(); }
+        size_type size() const noexcept { return _l.size(); }
+        size_type max_size() const noexcept { return _l.max_size(); }
 
         void clear() { _l.clear(); }
 
-        // insert
-        // erase
-        // push_back
-        // pop_back
-        // push_front
-        // pop_front
+        iterator insert(const_iterator pos, const value_type& value) { return _l.insert(pos, value); }
+        iterator insert(const_iterator pos, size_type count, const value_type& value) { return _l.insert(pos, count, value); }
+        template<class InputIt>
+        iterator insert(const_iterator pos, InputIt first, InputIt last) { return _l.insert<InputIt>(pos, first, last); }
 
-        // resize
-        // swap
+        iterator erase(iterator pos) { return _l.erase(pos); }
+        iterator erase(iterator first, iterator last) { return _l.erase(first, last); }
 
-        // merge
-        // splice
+        void push_back(const value_type& value) { _l.push_back(value); }
+        void pop_back() { _l.pop_back(); }
+        void push_front(const value_type& value) { _l.push_front(value); }
+        void pop_front() { _l.pop_front(); }
+
+        void resize(size_type count) { _l.resize(count); }
+        void resize(size_type count, const value_type& value) { _l.resize(count, value); }
+
+        void merge(list& other) { _l.merge(other._l); }
+        template<class Compare>
+        void merge(list& other, Compare comp) { _l.merge(other._l, comp); }
+
+        void splice(const_iterator pos, list& other) { _l.splice(pos, other); }
+        void splice(const_iterator pos, list& other, const_iterator it ) { _l.splice(pos, other, it); }
+        void splice(const_iterator pos, list& other, const_iterator first, const_iterator last) { _l.splice(pos, other, first, last); }
+
         // remove
         // remove_if
         // reverse
@@ -87,17 +100,39 @@ namespace Utilities {
         const_iterator cend() const noexcept { return _l.cend(); };
         const_iterator crbegin() const noexcept { return _l.crbegin(); };
         const_iterator crend() const noexcept { return _l.crend(); };
-        // emplace
-        // emplace_back
-        // emplace_front
+        iterator insert(const_iterator pos, value_type&& value) { return _l.insert(pos, value); }
+        iterator insert(const_iterator pos, std::initializer_list<value_type> ilist) { return _l.insert(pos, ilist); }
+        template<class... Args>
+        iterator emplace(const_iterator pos, Args&&... args) { return _l.emplace(pos, args...); }
+        template<class... Args>
+        iterator emplace_back(Args&&... args) { return _l.emplace_back(args...); }
+        template<class... Args>
+        iterator emplace_front(Args&&... args) { return _l.emplace_front(args...); }
+        iterator erase(const_iterator pos) { return _l.erase(pos); }
+        iterator erase(const_iterator first, const_iterator last) { return _l.erase(first, last); }
+
+        void push_back(value_type&& value) { _l.push_back(value); }
+        void push_front(value_type&& value) { _l.push_front(value); }
+
+        void merge(list&& other) { _l.merge(other._l); }
+        template<class Compare>
+        void merge(list&& other, Compare comp) { _l.merge(other._l, comp); }
+
+        void splice(const_iterator pos, list&& other) { _l.splice(pos, other); }
+        void splice(const_iterator pos, list&& other, const_iterator it ) { _l.splice(pos, other, it); }
+        void splice(const_iterator pos, list&& other, const_iterator first, const_iterator last) { _l.splice(pos, other, first, last); }
+#endif
+#if __cplusplus >= 201703L
+        void swap(list& other) noexcept { _l.swap(other._l); }
+#else
+        void swap(list& other) { _l.swap(other._l); }
 #endif
 #if __cplusplus >=202302L
-    // assign_range
+        // assign_range
         // insert_range
         // append_range
 #endif
-    }
-1}
-
+    };
+}
 
 #endif // UTILITIES_CONCURRENT_LIST_HPP
