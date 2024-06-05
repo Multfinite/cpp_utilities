dce6a7ee69519cc0326efe9241b16024145617bf
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-05 10:11:07 +0300

    Update optional.hpp

--- optional.hpp
+++ optional.hpp
@@ -6,11 +6,15 @@
 
     template<typename _Tp>
     using optional = std::optional<_Tp>;
+    using nullopt_t = std::nullopt_t;
+    constexpr nullopt_t nullopt { nullopt_t::_Construct::_Token };
 #elif STD_FS == 2
     #include <experimental/optional>
 
     template<typename _Tp>
     using optional = std::experimental::optional<_Tp>;
+    using nullopt_t = std::experimental::nullopt_t;
+    constexpr nullopt_t nullopt { nullopt_t::_Construct::_Token };
 #else
     static_assert (false, "<optional> not supported");
 #endif
