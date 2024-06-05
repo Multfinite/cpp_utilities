8a7ac92d7b57cb10c7775dbabe37790ec75b7f9d
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-05 13:06:08 +0300

    Update graph.hpp

--- graph.hpp
+++ graph.hpp
@@ -1,252 +1,246 @@
-#ifndef UTILITIES_GRAPH_HPP
-#define UTILITIES_GRAPH_HPP
+#ifndef UTILITIES_PATHFINDING_HPP
+#define UTILITIES_PATHFINDING_HPP
 
-#include <list>
-#include <vector>
-#include <iterator>
-#include <memory>
+#include "graph.hpp"
+#include "optional.hpp"
 
-#include "algorithm.hpp"
-#include "templates.hpp"
-#include "macro.hpp"
-#include "linking_node.hpp"
-
-namespace Utilities
+namespace Utilities::Pathfinding
 {
+    enum struct VertexState : unsigned char
+    {
+        Unhandled = 0,
+        Queued = 1,
+        Explored = 255
+    };
+
+    template<typename TVertex, typename TCostType = double>
 #if HAS_CONCEPTS
-    template<typename T>
-    concept IsVertex =
-        HasLinkingNode<T> &&
-        //std::is_same<decltype(T::__linked), typename T::linked_type>::value &&
-        requires(T x)
-    {
-        typename T::data_type;
-        typename T::linked_type;
-        x.Context;
-        std::is_same<decltype(x.__linked), typename T::linked_type>::value;
-        x.clear();
-    };
-
-    template<typename T>
-    concept IsEdgeCachedVertex =
-        std::ranges::range<typename T::edges_type> &&
-        std::ranges::range<typename T::vertexes_type> &&
-       IsVertex<T> && requires(T x)
-    {
-        typename T::edges_type;
-        typename T::vertexes_type;
-        x.Edges; x.IncomingEdges; x.OutcomingEdges;
-        x.Neighbors; x.IncomingNeighbors; x.OutcomingNeighbors;
-    };
-
-    template<typename T>
-    concept IsEdge = IsVertex<typename T::vertex_type> &&
-        HasLinkingNode<T> &&
-        //std::is_same<decltype(T::__linked), typename T::linked_type>::value &&
-        requires(T x, const typename T::vertex_type& v_, const typename T::data_type& d_)
-    {
-        typename T::data_type;
-        typename T::vertex_type;
-        typename T::linked_type;
-        x.Context;
-        std::is_same<decltype(x.__linked), typename T::linked_type>::value;
-        x.To;
-        x.From;
-        x = T(v_, v_);
-        x = T(v_, v_, d_);
-    };
-
-    template<typename T>
-    concept IsCachingEdge = IsEdgeCachedVertex<typename T::vertex_type> && IsEdge<T>;
-
-    template<typename T>
-    concept IsGraph =
-        HasLinkingNode<T> &&
-        IsVertex<typename T::vertex_type> &&
-        IsEdge<typename T::edge_type> &&
-        requires(T x)
-    {
-        x.NodeVertexes;
-        x.NodeEdges;
-        x.NodeGraph;
-    };
-
-    template<typename T>
-    concept IsCachingGraph =
-        HasLinkingNode<T> &&
-        IsEdgeCachedVertex<typename T::vertex_type> &&
-        IsCachingEdge<typename T::edge_type> &&
-        IsGraph<T>;
+        requires IsVertex<TVertex>&& IsNumericType<TCostType>
 #endif
-
-    template<typename TData>
-    struct Vertex
+    struct VertexNode : public __linked_node<TVertex>
     {
-        using data_type = TData;
-        using vertex_type = Vertex<data_type>;
-        using edge_ptr = void*;
-        using vertex_ptr = vertex_type*;
-        using edges_type = std::list<edge_ptr>;
-        using vertexes_type = std::list<vertex_ptr>;
+        using vertex_type = TVertex;
+        using cost_type = TCostType;
 
-        __linking_node __linking;
+        vertex_type* Previous;
+        VertexState State;
+        /*!
+         * @brief Total cost from root to this
+         */
+        optional<cost_type> Cost;
 
-        data_type Context;
-        edges_type Edges, IncomingEdges, OutcomingEdges;
-        vertexes_type Neighbors, IncomingNeighbors, OutcomingNeighbors;
+        VertexNode(const vertex_type& owner) : __linked_node<TVertex>(owner) { clear(); }
+        VertexNode(const vertex_type& owner, size_t index) : __linked_node<TVertex>(owner, index) { clear(); }
+        ~VertexNode() = default;
 
-        Vertex(const data_type& context) : Context(context) {}
-        Vertex() : Vertex(data_type{}) {}
+        template<typename TPathType = std::vector<std::reference_wrapper<vertex_type>>>
+        TPathType Build() const
+        {
+            TPathType path;
+            auto* v = &this;
+            while (v != nullptr)
+            {
+                path.pop_back(*v);
+                v = v->Pathfinding->Previous;
+            }
+            return path;
+        }
+
+        vertex_type& Root() const
+        {
+            auto* v = &this;
+            while (v != nullptr)
+                v = v->Pathfinding->Previous;
+            return *v;
+        }
 
         void clear()
         {
-            clear(
-              Edges, IncomingEdges, OutcomingEdges,
-              Neighbors, IncomingNeighbors, OutcomingNeighbors
-            );
+            Previous = nullptr;
+            State = VertexState::Unhandled;
+            Cost = nullopt;
         }
     };
 
-    /*!
-     * @brief one-directional edge
-     */
-    template<typename TVertex, typename TData>
+    template<typename TEdge, typename TCostType = double>
 #if HAS_CONCEPTS
-        requires IsEdgeCachedVertex<TVertex>
+        requires IsVertex<TVertex> && IsEdge<TEdge> && IsNumericType<TCostType>
 #endif
-    struct Edge
+    struct EdgeNode : public __linked_node<TEdge>
     {
-        using data_type = TData;
-        using vertex_type = TVertex;
-
-        __linking_node __linking;
-
-        data_type Context;
-        vertex_type& From;
-        vertex_type& To;
-
-        Edge(const vertex_type& from, const vertex_type& to, const data_type& context) :
-            From(from), To(to), Context(context)
-        {
-            From.Neighbors.push_back(&to); From.OutcomingNeighbors.push_back(&to);
-            From.Edges.push_back(this); From.OutcomingEdges.push_back(this);
-
-            To.Neighbors.push_back(&from); To.IncomingNeighbors.push_back(&from);
-            To.Edges.push_back(this); To.IncomingEdges.push_back(this);
-        }
-        Edge(const vertex_type& from, const vertex_type& to) : Edge(from, to, data_type{}) {}
-        ~Edge()
-        {
-             auto to = To; auto from = From;
-
-            From.Neighbors.remove(&to);  From.OutcomingNeighbors.remove(&to);
-            From.Edges.remove(this); From.OutcomingEdges.remove(this);
-
-            To.Neighbors.remove(&from); To.IncomingNeighbors.remove(&from);
-            To.Edges.remove(this); To.IncomingEdges.remove(this);
-        }
-    };
-
-    /*!
-     * @brief Bidirectional edge
-     */
-    template<typename TVertex, typename TData>
-#if HAS_CONCEPTS
-        requires IsEdgeCachedVertex<TVertex>
-#endif
-    struct EdgeBidirectional
-    {
-        using data_type = TData;
-        using vertex_type = TVertex;
-
-        __linking_node __linking;
-
-        data_type Context;
-        vertex_type& From;
-        vertex_type& To;
-
-        EdgeBidirectional(const vertex_type& from, const vertex_type& to, const data_type& context) :
-            From(from), To(to), Context(context)
-        {
-            From.Neighbors.push_back(&to);  From.IncomingNeighbors.push_back(&to); From.OutcomingNeighbors.push_back(&to);
-            From.Edges.push_back(this); From.IncomingEdges.push_back(this); From.OutcomingEdges.push_back(this);
-
-            To.Neighbors.push_back(&from); To.IncomingNeighbors.push_back(&from); To.OutcomingNeighbors.push_back(&from);
-            To.Edges.push_back(this); To.IncomingEdges.push_back(this); To.OutcomingEdges.push_back(this);
-        }
-        EdgeBidirectional(const vertex_type& from, const vertex_type& to) : EdgeBidirectional(from, to, data_type{}) {}
-        ~EdgeBidirectional()
-        {
-            auto to = To; auto from = From;
-
-            From.Neighbors.remove(&to);  From.IncomingNeighbors.remove(&to); From.OutcomingNeighbors.remove(&to);
-            From.Edges.remove(this); From.IncomingEdges.remove(this); From.OutcomingEdges.remove(this);
-
-            To.Neighbors.remove(&from); To.IncomingNeighbors.remove(&from); To.OutcomingNeighbors.remove(&from);
-            To.Edges.remove(this); To.IncomingEdges.remove(this); To.OutcomingEdges.remove(this);
-        }
-    };
-
-    template<typename TVertex, typename TEdge, typename TData>
-#if HAS_CONCEPTS
-        requires IsEdgeCachedVertex<TVertex> && IsCachingEdge<TEdge>
-#endif
-    class Graph
-    {
-    public:
-        using data_type = TData;
-        using vertex_type = TVertex;
         using edge_type = TEdge;
-        using edge_data_type = typename edge_type::data_type;
-        using vertexes_type = std::vector<vertex_type>;
-        using edges_type = std::vector<edge_type>;
+        using vertex_type = typename edge_type::vertex_type;
+        using cost_type = TCostType;
 
-        __linking_node __linking;
+        /*!
+         * @brief Atomic transferring cost of this edge
+         */
+        optional<cost_type> Cost;
 
-        data_type Context;
-        vertexes_type Vertexes;
-        edges_type Edges;
+        EdgeNode(const edge_type& owner) : __linked_node<TEdge>(owner) { clear(); }
+        EdgeNode(const edge_type& owner, size_t index) : __linked_node<TEdge>(owner, index) { clear(); }
+        ~EdgeNode() = default;
 
-        Graph() = default;
-        Graph(size_t vertices)
+        void clear()
         {
-            Vertexes.resize(vertices);
+            Cost = nullopt;
         }
+    };
 
-        static edge_type& EdgeTo(vertex_type& from, vertex_type& to)
-        {
-            return *find(to.IncomingNeighbors, from);
-        }
-        edge_type& Between(const vertex_type& from, const vertex_type& to)
-        {
-            return Between(from, to, edge_data_type{});
-        }
-        edge_type& Between(const vertex_type& from, const vertex_type& to, const edge_data_type& context)
-        {
-            try
-            {
-                return *find(to.IncomingNeighbors, from);
-            }
-            catch (const Exceptions::item_not_found_exception&)
-            {
-                return Edges.emplace_back(from, to, context);
-            }
-        }
+    template<typename TGraph, typename TCostType = double>
+#if HAS_CONCEPTS
+        requires IsGraph<TGraph> && IsNumericType<TCostType>
+#endif
+    struct GraphNode : public __linked_node<TGraph>
+    {
+        using cost_type = TCostType;
+        using graph_type = TGraph;
+        using vertex_type = typename graph_type::vertex_type;
+        using edge_type = typename graph_type::edge_type;
+        using vertex_node_type = VertexNode<vertex_type>;
+        using edge_node_type = EdgeNode<edge_type>;
 
-        void RemoveEdgesOf(vertex_type& vertex)
+        std::vector<vertex_node_type> Vertexes;
+        std::vector<edge_node_type> Edges;
+
+        GraphNode(const graph_type& owner) : __linked_node<TGraph>(owner) { init(); }
+        GraphNode(const graph_type& owner, size_t index) : __linked_node<TGraph>(owner, index) { init(); }
+        ~GraphNode() = default;
+
+        void init()
         {
-             for(auto& edge : vertex.Edges())
-                 Edges.remove(&edge);
-             vertex.clear();
+            Vertexes.reserve(this->Owner.Vertexes.size());
+            for (auto& v : this->Owner.Vertexes)
+                Vertexes.emplace_back(v, this->Index);
+
+            Edges.reserve(this->Owner.Edges.size());
+            for (auto& e : this->Owner.Edges)
+                Edges.emplace_back(e, this->Index);
         }
 
         void clear()
         {
-            __linking.clear();
             Edges.clear();
             Vertexes.clear();
+        }
+
+        inline vertex_node_type& node_of(const vertex_type& v) const
+        {
+            return v.__linking.template as_ref<vertex_node_type>(this->Index);
+        }
+        inline edge_node_type& node_of(const edge_type& v) const
+        {
+            return v.__linking.template as_ref<vertex_node_type>(this->Index);
+        }
+    };
+#if HAS_CONCEPTS
+    template<typename T>
+    concept IsPathfinding = requires(T x)
+    {
+        x._linked_index;
+        std::is_same<decltype(x._linked_index), size_t>::value;
+        x.Graph;
+        x.GraphNode;
+    };
+
+    template<typename T, typename TVertex, typename TCostType>
+    concept IsCostEvaluator =
+        IsVertex<TVertex> && IsNumericType<TCostType> &&
+        requires(T x, TVertex v, TCostType c)
+    {
+        c = x(v);
+    };
+#endif
+
+    template<typename TGraphNode, typename TEdgeNode = typename TGraphNode::edge_type, typename TCostType = typename TGraphNode::cost_type>
+#if HAS_CONCEPTS
+        requires IsVertex<TVertex>&& IsNumericType<TCostType>
+#endif
+    TCostType EvaluateCostDefault(const TGraphNode& g, const TEdgeNode& e) { return 1; }
+
+    template<
+        typename TGraph,
+        typename TPathType,
+        typename TCostEvaluator,
+        typename TCostType = double
+    >
+#if HAS_CONCEPTS
+    requires
+        IsCachingGraph<TGraph>
+#endif
+    class BFS
+    {
+    public:
+        using cost_type = TCostType;
+        using graph_type = TGraph;
+        using vertex_type = typename graph_type::vertex_type;
+        using vertex_ptr = vertex_type*;
+        using edge_type = typename graph_type::edge_type;
+        using edge_ptr = edge_type*;
+        using path_type = TPathType;
+        using vertexes_type = std::list<std::reference_wrapper<vertex_type>>;
+        using graph_node_type = GraphNode<TGraph, cost_type>;
+        using vertex_node_type = typename graph_node_type::vertex_node_type;
+        using edge_node_type = typename graph_node_type::edge_node_type;
+
+        const graph_type& Graph;
+        graph_node_type GraphNode;
+
+        TCostEvaluator Evaluator;
+
+        BFS(const graph_type& graph, const TCostEvaluator& evaluator)
+            : Graph(graph), GraphNode(graph), Evaluator(evaluator)
+        {}
+        BFS(const graph_type& graph) : BFS(graph, TCostEvaluator{}) {}
+        ~BFS() = default;
+
+        path_type operator()(const vertex_type& from, const vertex_type& to)
+        {
+            for (auto& e : GraphNode.Edges)
+            {
+                e.clear();
+                e.Cost = Evaluator(GraphNode, e);
+            }
+
+            GraphNode.node_of(from).State = VertexState::Explored;
+            GraphNode.node_of(from).Cost = 0;
+
+            vertexes_type reachable;
+            reachable.push_back(from);
+            while (!reachable.empty())
+            {
+                vertex_type& current = reachable.front(); reachable.pop_front();
+                vertex_node_type& nodeCurrent = GraphNode.node_of(current);
+                if (current == to)
+                    return nodeCurrent.Build();
+
+                reachable.remove(current);
+                nodeCurrent.State = VertexState::Explored;
+
+                for (auto& pEdge : current.OutcomingEdges)
+                {
+                    auto& edge = reference_cast<edge_type>(pEdge);
+                    auto& nodeEdge = GraphNode.node_of(edge);
+                    auto& adjacent = edge.To;
+                    auto& nodeAdjacent = GraphNode.node_of(adjacent);
+
+                    if (nodeAdjacent.State == VertexState::Unhandled)
+                    {
+                        nodeAdjacent.State = VertexState::Queued;
+                        reachable.push_back(adjacent);
+                    }
+                    constexpr bool isCostPresent = nodeAdjacent.Cost.has_value();
+                    constexpr bool isShorter = nodeCurrent.Cost + nodeEdge.Cost < nodeAdjacent.Cost;
+                    if (!isCostPresent || isShorter)
+                    {
+                        nodeAdjacent.Previous = current;
+                        nodeAdjacent.Cost = nodeCurrent.Cost + nodeEdge.Cost;
+                    }
+                }
+            }
+            return path_type{};
         }
     };
 }
 
-#endif // UTILITIES_GRAPH_HPP
+#endif // UTILITIES_PATHFINDING_HPP
