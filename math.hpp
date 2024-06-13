4c76c19f98a759322a25e13b83cf51283997b05d
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-13 15:32:29 +0300

    Create math.hpp

---
+++ math.hpp
@@ -0,0 +1,23 @@
+#ifndef UTILITIES_MATH_HPP
+#define UTILITIES_MATH_HPP
+
+#include <cmath>
+
+namespace Utilities::Math
+{
+    template<typename TNumeric>
+    struct Vector2
+    {
+        using value_type = TNumeric;
+        using vector_type = Vector2<value_type>;
+
+        value_type X, Y;
+
+        vector_type operator+(vector_type const& other) const { return vector_type { X + other.X, Y + other.Y }; };
+        vector_type operator-(vector_type const& other) const { return vector_type { X - other.X, Y - other.Y }; };
+        vector_type operator*(TNumeric scalar) const { return vector_type { X * scalar, Y * scalar }; };
+        vector_type operator/(TNumeric scalar) const { return vector_type { X / scalar, Y / scalar }; };
+    };
+}
+
+#endif // UTILITIES_MATH_HPP
