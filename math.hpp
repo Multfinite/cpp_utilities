b2fae270bffb594fdcfd62fffad2faf040ae3352
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-14 07:25:08 +0300

    Update math.hpp

--- math.hpp
+++ math.hpp
@@ -17,6 +17,10 @@ namespace Utilities::Math
         vector_type operator-(vector_type const& other) const { return vector_type { X - other.X, Y - other.Y }; };
         vector_type operator*(TNumeric scalar) const { return vector_type { X * scalar, Y * scalar }; };
         vector_type operator/(TNumeric scalar) const { return vector_type { X / scalar, Y / scalar }; };
+
+        vector_type max(vector_type const& other) const { return vector_type { std::max(X, other.X), std::max(Y, other.Y) }; }
+        vector_type min(vector_type const& other) const { return vector_type { std::min(X, other.X), std::min(Y, other.Y) }; }
+        vector_type abs(vector_type const& other) const { return vector_type { std::abs(X, other.X), std::abs(Y, other.Y) }; }
     };
 }
 
