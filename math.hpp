41b26b0afa80a674c7f92f890e20c9ae608b5b70
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-14 09:22:15 +0300

    Update math.hpp

--- math.hpp
+++ math.hpp
@@ -11,6 +11,8 @@ namespace Utilities::Math
         using value_type = TNumeric;
         using vector_type = Vector2<value_type>;
 
+        static constexpr vector_type Zero = vector_type { 0, 0 };
+
         value_type X, Y;
 
         vector_type operator+(vector_type const& other) const { return vector_type { X + other.X, Y + other.Y }; };
@@ -18,9 +20,9 @@ namespace Utilities::Math
         vector_type operator*(TNumeric scalar) const { return vector_type { X * scalar, Y * scalar }; };
         vector_type operator/(TNumeric scalar) const { return vector_type { X / scalar, Y / scalar }; };
 
-        vector_type max(vector_type const& other) const { return vector_type { std::max(X, other.X), std::max(Y, other.Y) }; }
-        vector_type min(vector_type const& other) const { return vector_type { std::min(X, other.X), std::min(Y, other.Y) }; }
-        vector_type abs(vector_type const& other) const { return vector_type { std::abs(X, other.X), std::abs(Y, other.Y) }; }
+        static vector_type max(vector_type const& a, vector_type const& b) { return vector_type { std::max(a.X, b.X), std::max(a.Y, b.Y) }; }
+        static vector_type min(vector_type const& a, vector_type const& b) { return vector_type { std::min(a.X, b.X), std::min(a.Y, b.Y) }; }
+        static vector_type abs(vector_type const& a, vector_type const& b) { return vector_type { std::abs(a.X, b.X), std::abs(a.Y, b.Y) }; }
     };
 }
 
