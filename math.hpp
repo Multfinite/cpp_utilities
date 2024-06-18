c4112067c9dc557dc192fa998fa4904788a87e5d
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-18 07:43:30 +0300

    Update math.hpp

--- math.hpp
+++ math.hpp
@@ -22,6 +22,10 @@ namespace Utilities::Math
         bool operator==(vector_type const& other) const { return X == other.X && Y == other.Y; };
         bool operator!=(vector_type const& other) const { return X == other.X && Y == other.Y; };
 
+        vector_type ceil() const { return vector_type { std::ceil(X), std::ceil(Y) }; };
+        vector_type round() const { return vector_type { std::round(X), std::round(Y) }; };
+        vector_type floor() const { return vector_type { std::floor(X), std::floor(Y) }; };
+
         static vector_type max(vector_type const& a, vector_type const& b) { return vector_type { std::max(a.X, b.X), std::max(a.Y, b.Y) }; }
         static vector_type min(vector_type const& a, vector_type const& b) { return vector_type { std::min(a.X, b.X), std::min(a.Y, b.Y) }; }
         static vector_type abs(vector_type const& a, vector_type const& b) { return vector_type { std::abs(a.X, b.X), std::abs(a.Y, b.Y) }; }
