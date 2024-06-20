b9b3ebd2f558c6e81f75517a44b4cd361c41df3f
Mikhail Kuznetcov <multfinite@gmail.com>
2024-06-20 09:22:57 +0300

    Update filesystem.hpp

--- filesystem.hpp
+++ filesystem.hpp
@@ -11,4 +11,10 @@
     static_assert (false, "<filesystem> not supported");
 #endif
 
+namespace Utilities
+{
+    inline fs::path absolute(fs::path const& p) { return p.is_absolute() ? p : (fs::current_path() / p ); }
+    inline fs::path absolute(std::string const& p) { return fs::path(p).is_absolute() ? p : (fs::current_path() / p ); }
+}
+
 #endif // UTILITIES_FILESYSTEM_HPP
