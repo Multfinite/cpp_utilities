d231f322b8760ec0e787f2f1ca18ee37586802c3
Mikhail Kuznetcov <multfinite@gmail.com>
2024-07-10 08:19:54 +0300

    Update math.hpp

--- math.hpp
+++ math.hpp
@@ -20,6 +20,7 @@ namespace Utilities::Math
 
         vector_type operator+(vector_type const& other) const { return vector_type { X + other.X, Y + other.Y }; };
         vector_type operator-(vector_type const& other) const { return vector_type { X - other.X, Y - other.Y }; };
+        vector_type operator-() const { return vector_type { -X, -Y }; }
         vector_type operator*(TNumeric scalar) const { return vector_type { X * scalar, Y * scalar }; };
         vector_type operator/(TNumeric scalar) const { return vector_type { X / scalar, Y / scalar }; };
         bool operator==(vector_type const& other) const { return X == other.X && Y == other.Y; };
@@ -32,6 +33,11 @@ namespace Utilities::Math
         static vector_type max(vector_type const& a, vector_type const& b) { return vector_type { std::max(a.X, b.X), std::max(a.Y, b.Y) }; }
         static vector_type min(vector_type const& a, vector_type const& b) { return vector_type { std::min(a.X, b.X), std::min(a.Y, b.Y) }; }
         static vector_type abs(vector_type const& a, vector_type const& b) { return vector_type { std::abs(a.X, b.X), std::abs(a.Y, b.Y) }; }
+
+        template<typename TValueTypeTo>
+        inline static Vector2<TValueTypeTo> static_cast_to(vector_type const& v) { return Vector2<TValueTypeTo> { static_cast<TValueTypeTo>(v.X), static_cast<TValueTypeTo>(v.Y) }; }
+        template<typename TValueTypeFrom>
+        inline static vector_type static_cast_from(Vector2<TValueTypeFrom> const& v) { return vector_type { static_cast<value_type>(v.X), static_cast<value_type>(v.Y) }; }
     };
 }
 
