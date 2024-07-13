9cf5eee8f3d74574204adb1575c462c8cf41edd4
Mikhail Kuznetcov <multfinite@gmail.com>
2024-07-13 11:03:46 +0300

    Update string.enum.hpp

--- string.enum.hpp
+++ string.enum.hpp
@@ -1,61 +1,120 @@
-#ifndef UTILITIES_STRING_ENUM_HPP
-#define UTILITIES_STRING_ENUM_HPP
+#ifndef UTILITIES_MACRO_HPP
+#define UTILITIES_MACRO_HPP
 
-#include <map>
+/* https://stackoverflow.com/a/56483887 */
 
-#include "string.hpp"
-#include "algorithm.hpp"
-#include "exceptions.hpp"
+#define CPP98 199711L
+#define CPP03 CPP98
+#define CPP11 201103L
+#define CPP14 201402L
+#define CPP17 201703L
+#define CPP20 202002L
+#define CPP23 202302L
 
-namespace Utilities::Enum
+#define CPP_SINCE(x, H) x >= H
+#define CPP_REMOVED(x, H) x < H
+#define CPP_UNTIL(x, H) x < H
+#define CPP_BETWEEN(x, L, H) x < H && x >= L
+
+#include <cstdlib>
+#include <type_traits>
+#include <initializer_list>
+
+template<typename T>
+inline T& reference_cast(T* ptr)
 {
-    template <typename T>
-    std::map<std::string, T> GetMap();
-
-    template<typename T>
-    T from_string(const std::string& s)
-    {
-        try {
-            std::map<std::string, T> m = GetMap<T>();
-            return m.at(Utilities::string_tolower(s));
-        } catch (...) {
-            throw construct_error(Exceptions::base_error, "from_string<T = " + std::string(typeid(T).name()) + "> invalid string value v = { " + s + " }");
-        }
-    }
-
-    template<typename T>
-    std::string to_string(T v)
-    {
-         std::map<std::string, T> m = GetMap<T>();
-         auto iter = Utilities::find_if(m, [&v](const typename decltype(m)::value_type& x)
-         {
-            return x.second == v;
-         });
-         if(iter != m.end())
-             return iter->first;
-
-         throw construct_error(Exceptions::base_error, "to_string<T = " + std::string(typeid(T).name()) + "> invalid value of enum v = { " + std::to_string(static_cast<std::underlying_type_t<T>>(v)) + " }");
-    }
+    if(!ptr) throw "POINTER IS NULL";
+    return *ptr;
+}
+template<typename T>
+inline T& reference_cast(void* ptr)
+{
+    if(!ptr) throw "POINTER IS NULL";
+    return *reinterpret_cast<T*>(ptr);
+}
+template<typename T>
+inline T const& reference_cast(T const* ptr)
+{
+    if(!ptr) throw "POINTER IS NULL";
+    return *ptr;
+}
+template<typename T>
+inline T const& reference_cast(void const* ptr)
+{
+    if(!ptr) throw "POINTER IS NULL";
+    return *reinterpret_cast<T const*>(ptr);
 }
 
-#define enum_map_pair(enum, enum_value) { nameof(enum_value), enum::enum_value }
+template<typename T>
+inline T* offset_ptr(void* ptr, size_t offset = 0)
+{
+    unsigned char* pointer = static_cast<unsigned char*>(ptr) + offset;
+    return reinterpret_cast<T*>(pointer);
+}
 
-#if ENABLE_NLOHMANN_JSON == 1
-#define JSON_ENUM_TYPE_CONVERSION(enum_type) \
-    void to_json(nlohmann::json& j, const enum_type& v) { j = to_string(v); } \
-    void from_json(const nlohmann::json& j, enum_type& v) \
-    { \
-        if(j.is_string()) \
-            v = from_string<std::remove_reference_t<decltype(v)>>((std::string) j); \
-        else if(j.is_number_integer()) \
-            v =  static_cast<std::remove_reference_t<decltype(v)>>((long) j); \
-        else if(j.is_number_unsigned()) \
-            v = static_cast<std::remove_reference_t<decltype(v)>>((unsigned long) j); \
-        else throw construct_error(Exceptions::base_error, "j is not string or int\\uint. ONLY strings and integers are allowed."); \
-    } \
+#if CPP_SINCE(__cplusplus, CPP20)
+template<typename T>
+concept Clearable = requires(T x) { x.clear(); };
+#endif
 
-#else
-#define JSON_ENUM_TYPE_CONVERSION(enum_type)
-#endif // ENABLE_NLOHMANN_JSON
+template<typename ...T>
+#if  CPP_SINCE(__cplusplus, CPP20)
+        requires (Clearable<T> && ...)
+#endif
+inline void clear(const T& ...args)
+{
+	for (auto& x : {args...})
+		x.clear();
+}
 
-#endif // UTILITIES_STRING_ENUM_HPP
+template<typename T>
+inline bool is_any_of(T&& value, std::initializer_list<T> variants)
+{
+    for(T& variant : variants)
+        if(variant == value)
+            return  true;
+    return false;
+}
+
+template<typename T, typename TComparer>
+inline bool is_any_of(T&& value, std::initializer_list<T> variants, TComparer&& comp)
+{
+    for(T& variant : variants)
+        if(comp(variant, value))
+            return  true;
+    return false;
+}
+
+template <typename TIterator>
+inline auto value_of_iterator(TIterator const& x, TIterator const& end)
+{
+    using T = typename TIterator::value_type;
+    if constexpr(std::is_pointer_v<T>)
+         return x == end ? *x : nullptr;
+    else
+         return x == end ? &*x : nullptr;
+}
+
+#define GETTER_NAME(name) get_##name
+#define SETTER_NAME(name) set_##name
+
+#define GETTER_DECL(name, type) type GETTER_NAME(name)()
+#define SETTER_DECL(name, type) void SETTER_NAME(name)(type value)
+
+#define GETTER_V(name, field) std::remove_reference_t<decltype(field)> GETTER_NAME(name)()
+#define GETTER_R(name, field) std::remove_reference_t<decltype(field)>& GETTER_NAME(name)()
+#define GETTER_RC(name, field) std::remove_reference_t<decltype(field)> const& GETTER_NAME(name)()
+
+#define SETTER_V(name, field) void SETTER_NAME(name)(std::remove_reference_t<decltype(field)> value)
+#define SETTER_R(name, field) void SETTER_NAME(name)(std::remove_reference_t<decltype(field)>& value)
+#define SETTER_RC(name, field) void SETTER_NAME(name)(std::remove_reference_t<decltype(field)> const& value)
+
+#define GETTER_V_DEFAULT(name, field) GETTER_V(name, field) const { return field; }
+#define GETTER_R_DEFAULT(name, field) GETTER_R(name, field) const { return field; }
+#define GETTER_RC_DEFAULT(name, field) GETTER_RC(name, field) const { return field; }
+
+#define SETTER_V_DEFAULT(name, field) SETTER_V(name, field) { field = value; }
+#define SETTER_R_DEFAULT(name, field) SETTER_R(name, field) { field = value; }
+#define SETTER_RC_DEFAULT(name, field) SETTER_RC(name, field) { field = value; }
+
+#endif //UTILITIES_MACRO_HPP
