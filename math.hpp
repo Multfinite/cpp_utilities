c434aa81d1cd13d8ba2291f6e354cbcd35826394
Mikhail Kuznetcov <multfinite@gmail.com>
2024-08-05 08:20:01 +0300

    Update math.hpp

--- math.hpp
+++ math.hpp
@@ -8,7 +8,7 @@
 namespace Utilities::Math
 {
     template<typename TNumeric>
-    inline TNumeric fraction(TNumeric const& x) {
+    inline TNumeric fraction(TNumeric const& x) noexcept {
         return fraction_inlined(x);
     }
 
@@ -18,36 +18,36 @@ namespace Utilities::Math
         using value_type = TNumeric;
         using vector_type = Vector2<value_type>;
 
-        inline static vector_type Zero() { return vector_type { 0, 0 }; };
-        inline static vector_type Identity() { return vector_type { 1, 1 }; };
-        inline static vector_type XAxis() { return vector_type { 1, 0 }; };
-        inline static vector_type YAxis() { return vector_type { 0, 1 }; };
+        inline static vector_type Zero() noexcept { return vector_type { 0, 0 }; };
+        inline static vector_type Identity() noexcept { return vector_type { 1, 1 }; };
+        inline static vector_type XAxis() noexcept { return vector_type { 1, 0 }; };
+        inline static vector_type YAxis() noexcept { return vector_type { 0, 1 }; };
 
         value_type X, Y;
 
-        inline vector_type operator+(vector_type const& other) const { return vector_type { X + other.X, Y + other.Y }; };
-        inline vector_type operator-(vector_type const& other) const { return vector_type { X - other.X, Y - other.Y }; };
-        inline vector_type operator-() const { return vector_type { -X, -Y }; }
-        inline vector_type operator*(TNumeric scalar) const { return vector_type { X * scalar, Y * scalar }; };
-        inline vector_type operator/(TNumeric scalar) const { return vector_type { X / scalar, Y / scalar }; };
-        inline bool operator==(vector_type const& other) const { return X == other.X && Y == other.Y; };
-        inline bool operator!=(vector_type const& other) const { return X == other.X && Y == other.Y; };
+        inline vector_type operator+(vector_type const& other) const noexcept { return vector_type { X + other.X, Y + other.Y }; };
+        inline vector_type operator-(vector_type const& other) const noexcept { return vector_type { X - other.X, Y - other.Y }; };
+        inline vector_type operator-() const { return vector_type noexcept { -X, -Y }; }
+        inline vector_type operator*(TNumeric scalar) const noexcept { return vector_type { X * scalar, Y * scalar }; };
+        inline vector_type operator/(TNumeric scalar) const noexcept { return vector_type { X / scalar, Y / scalar }; };
+        inline bool operator==(vector_type const& other) const noexcept { return X == other.X && Y == other.Y; };
+        inline bool operator!=(vector_type const& other) const noexcept { return X == other.X && Y == other.Y; };
 
-        inline vector_type ceil() const { return vector_type { std::ceil(X), std::ceil(Y) }; };
-        inline vector_type round() const { return vector_type { std::round(X), std::round(Y) }; };
-        inline vector_type floor() const { return vector_type { std::floor(X), std::floor(Y) }; };
+        inline vector_type ceil() const noexcept { return vector_type { std::ceil(X), std::ceil(Y) }; };
+        inline vector_type round() const noexcept { return vector_type { std::round(X), std::round(Y) }; };
+        inline vector_type floor() const noexcept { return vector_type { std::floor(X), std::floor(Y) }; };
 
-        inline TNumeric length_squared() const { return X * X + Y * Y; }
-        inline TNumeric length() const { return sqrt(length_squared()); }
+        inline TNumeric length_squared() const noexcept { return X * X + Y * Y; }
+        inline TNumeric length() const noexcept { return sqrt(length_squared()); }
 
-        static vector_type max(vector_type const& a, vector_type const& b) { return vector_type { std::max(a.X, b.X), std::max(a.Y, b.Y) }; }
-        static vector_type min(vector_type const& a, vector_type const& b) { return vector_type { std::min(a.X, b.X), std::min(a.Y, b.Y) }; }
-        static vector_type abs(vector_type const& a, vector_type const& b) { return vector_type { std::abs(a.X, b.X), std::abs(a.Y, b.Y) }; }
+        static vector_type max(vector_type const& a, vector_type const& b) noexcept { return vector_type { std::max(a.X, b.X), std::max(a.Y, b.Y) }; }
+        static vector_type min(vector_type const& a, vector_type const& b) noexcept { return vector_type { std::min(a.X, b.X), std::min(a.Y, b.Y) }; }
+        static vector_type abs(vector_type const& a, vector_type const& b) noexcept { return vector_type { std::abs(a.X, b.X), std::abs(a.Y, b.Y) }; }
 
         template<typename TValueTypeTo>
-        inline static Vector2<TValueTypeTo> static_cast_to(vector_type const& v) { return Vector2<TValueTypeTo> { static_cast<TValueTypeTo>(v.X), static_cast<TValueTypeTo>(v.Y) }; }
+        inline static Vector2<TValueTypeTo> static_cast_to(vector_type const& v) noexcept { return Vector2<TValueTypeTo> { static_cast<TValueTypeTo>(v.X), static_cast<TValueTypeTo>(v.Y) }; }
         template<typename TValueTypeFrom>
-        inline static vector_type static_cast_from(Vector2<TValueTypeFrom> const& v) { return vector_type { static_cast<value_type>(v.X), static_cast<value_type>(v.Y) }; }
+        inline static vector_type static_cast_from(Vector2<TValueTypeFrom> const& v) noexcept { return vector_type { static_cast<value_type>(v.X), static_cast<value_type>(v.Y) }; }
     };
 
     /*
@@ -59,7 +59,7 @@ namespace Utilities::Math
             TNumeric begin0, TNumeric end0
           , TNumeric begin1, TNumeric end1
           , TNumeric& beginOut, TNumeric& endOut
-    ) {
+    ) noexcept {
         // some rules:
         // 1. line 0 must be 'higher' than line 1
         // 2. begin 'point' must be lower than end 'point'
