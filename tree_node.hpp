0dc942b913814d385ec1f865749fd7372c4816ce
Mikhail Kuznetcov <multfinite@gmail.com>
2024-11-28 10:43:30 +0300

    Create tree_node.hpp

---
+++ tree_node.hpp
@@ -0,0 +1,47 @@
+#ifndef UTILITIES_TREE_NODE_HPP
+#define UTILITIES_TREE_NODE_HPP
+
+#include "macro.hpp"
+#include "events.hpp"
+#include <list>
+
+namespace Utilities
+{
+    class TreeNode
+    {
+        TreeNode* _parent = nullptr;
+        std::list<TreeNode*> _chlidrens;
+    public:
+        struct events_t
+        {
+            ObjectEvent<TreeNode, TreeNode* /*old parent */> ParentChanged;
+
+            events_t(TreeNode& node) : ParentChanged(node) {}
+        } Event;
+
+        bool is_root() const { return !_parent; }
+
+        GETTER_V_DEFAULT(parent, _parent)
+        SETTER_V(parent, _parent)
+        {
+            auto* old = _parent;
+            if(_parent) _parent->_chlidrens.remove(this);
+            _parent = value;
+            if(_parent) _parent->_chlidrens.push_back(this);
+            Event.ParentChanged(std::move(old));
+        }
+
+        template<typename T>
+        constexpr T* parent_as() const noexcept { return reinterpret_cast<T*>(_parent); }
+
+        auto begin() const { return _chlidrens.begin(); }
+        auto end() const { return _chlidrens.end(); }
+
+        auto cbegin() const { return _chlidrens.cbegin(); }
+        auto cend() const { return _chlidrens.cend(); }
+
+        TreeNode() : Event(*this) {}
+    };
+}
+
+#endif // UTILITIES_TREE_NODE_HPP
