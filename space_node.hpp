37b1f5e49c4f4d1bd7c38434fa04fdf254d8cb6b
Mikhail Kuznetcov <multfinite@gmail.com>
2024-11-28 10:50:51 +0300

    Update space_node.hpp

--- space_node.hpp
+++ space_node.hpp
@@ -1,14 +1,12 @@
 #ifndef UTILITIES_SPACE_NODE_HPP
 #define UTILITIES_SPACE_NODE_HPP
 
-#include <macro.hpp>
-
-#include <list>
+#include "tree_node.hpp"
 
 namespace Utilities
-{
+{    
     template<typename TVector>
-    class SpaceNode
+    class SpaceNode : public TreeNode
     {
         using node = SpaceNode<TVector>;
        /*!
@@ -17,45 +15,46 @@ namespace Utilities
         */
         using vector_type = TVector;
 
-        SpaceNode* _parent = nullptr;
-        std::list<std::reference_wrapper<node>> _chlidrens;
         vector_type _position, _globalPosition;
-
     public:
-        bool is_root() const { return !_parent; }
+        struct events_t
+        {
+            ObjectEvent<node, vector_type /* old position */> PositionChanged;
+
+            events_t(TreeNode& node) : PositionChanged(node) {}
+        } Event;
 
         GETTER_V_DEFAULT(global_position, _globalPosition)
         SETTER_V(global_position, _globalPosition)
         {
+            auto old = _position;
             _globalPosition = value;
-            _position = _globalPosition - (_parent ? _parent->_globalPosition : vector_type{});
-            for(node& chlid : _chlidrens) chlid.set_position(chlid._position);
+            _position = _globalPosition - (parent() ? parent_as<node>()->_globalPosition : vector_type{});
+            for(TreeNode* chlid : *this)
+                if(node* n = dynamic_cast<node*>(chlid))
+                    n->set_position(n->_position);
+            Event.PositionChanged(old);
         }
 
         GETTER_V_DEFAULT(position, _position)
         SETTER_V(position, _position)
         {
+            auto old = _position;
             _position = value;
-            _globalPosition = _position + (_parent ? _parent->_globalPosition : vector_type{});
-            for(node& chlid : _chlidrens) chlid.set_position(chlid._position);
+            _globalPosition = _position + (parent() ? parent_as<node>()->_globalPosition : vector_type{});
+            for(TreeNode* chlid : *this)
+                if(node* n = dynamic_cast<node*>(chlid))
+                    n->set_position(n->_position);
+            Event.PositionChanged(old);
         }
 
-        GETTER_V_DEFAULT(parent, _parent)
-        SETTER_V(parent, _parent)
+        SpaceNode() : TreeNode(), Event(*this)
         {
-            if(_parent) _parent->_chlidrens.remove_if([this](auto const& x) { return &x.get() == this; });
-            _parent = value;
-            if(_parent) _parent->_chlidrens.push_back(*this);
-            set_position(_position);
+            TreeNode::Event.ParentChanged += [this](TreeNode& sender, TreeNode* old)
+            {
+                set_position(_position);
+            };
         }
-
-        auto begin() const { return _chlidrens.begin(); }
-        auto end() const { return _chlidrens.end(); }
-
-        auto cbegin() const { return _chlidrens.cbegin(); }
-        auto cend() const { return _chlidrens.cend(); }
-
-        virtual ~SpaceNode() = default;
     };
 }
 
