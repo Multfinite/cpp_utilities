da6e64b362110811972ebca66a2cd6ab8c68fdb6
Mikhail Kuznetcov <multfinite@gmail.com>
2025-03-07 10:04:09 +0300

    Create cairo.hpp

---
+++ cairo/cairo.hpp
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_CAIRO_CAIRO_HPP
+#define UTILITIES_CAIRO_CAIRO_HPP
+
+#include "line.hpp"
+#include "polygon.hpp"
+#include "rect.hpp"
+#include "circle.hpp"
+#include "text.hpp"
+#include "gfx_entry.hpp"
+#include "svg.hpp"
+
+#endif // UTILITIES_CAIRO_CAIRO_HPP
