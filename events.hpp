0e99bc2d8c20c0c4d2fca59e725b4f196a5698b3
Mikhail Kuznetcov <multfinite@gmail.com>
2025-03-22 10:07:36 +0300

    Update events.hpp

--- events.hpp
+++ events.hpp
@@ -5,82 +5,101 @@
 #include <functional>
 #include <utility>
 #include <stdexcept>
+#include <memory>
 
 namespace Utilities
-{    
-        template<typename ...TArgs>
-	class Event
-	{
-	public:
-                using CallbackType = std::function<void(TArgs ...args)>;
-	private:
-		std::list<CallbackType> _callbacks;
-	public:
-		Event() = default;
-		~Event() { _callbacks.clear(); }
+{
+    template<typename ...TArgs>
+    class static_event_t final
+    {
+    public:
+        using callback_t = std::function<void(TArgs ...args)>;
+        using callback_ptr = std::shared_ptr<callback_t>;
+    private:
+        std::list<callback_ptr> _callbacks;
+    public:
+        static_event_t() = default;
+        ~static_event_t() { _callbacks.clear(); }
 
-                void operator()(TArgs... args) const
-		{
-			for (auto& callback : _callbacks)
-				callback(args...);
-		}
-		void operator+=(CallbackType callback) { _callbacks.push_back(callback); }
-		void operator-=(CallbackType callback) { _callbacks.remove(callback); }
-	};
+        void operator()(TArgs&&... args) const
+        {
+            for (callback_ptr& callback : _callbacks)
+                (*callback)(args...);
+        }
+        callback_ptr operator+=(callback_t&& callback) noexcept {
+            return _callbacks.emplace_back(callback);
+        }
+        callback_ptr operator+=(callback_t const& callback) noexcept {
+            return _callbacks.emplace_back(callback);
+        }
+        void operator-=(callback_t&& callback) noexcept {
+            _callbacks.remove(callback);
+        }
+        void operator-=(callback_t const& callback) noexcept {
+            _callbacks.remove(callback);
+        }
+    };
 
-        template<typename TObject, typename ...TArgs>
-	class ObjectEvent
-	{
-	public:
-                struct sender_must_be_valid_reference_error : std::runtime_error
-                {
-                    sender_must_be_valid_reference_error() :  std::runtime_error("nullptr was passed.") {}
-                };
+    template<typename TObject, typename ...TArgs>
+    class event_t final
+    {
+    public:
+        struct sender_must_be_valid_reference_error : std::runtime_error
+        {
+            sender_must_be_valid_reference_error() :  std::runtime_error("nullptr was passed.") {}
+        };
 
-                using CallbackType = std::function<void(TObject& sender, TArgs ...args)>;
-	private:
-                TObject* _sender;
-		std::list<CallbackType> _callbacks;
-	public:
-                ObjectEvent(TObject* sender) :
-                        _sender(sender)
-                {
-                    if(!_sender)
-                        throw sender_must_be_valid_reference_error{};
-                }
-                ObjectEvent(TObject& sender) noexcept :
-                        _sender(&sender)
-		{ }
+        using callback_t = std::function<void(TObject& sender, TArgs ...args)>;
+        using callback_ptr = std::shared_ptr<callback_t>;
+    private:
+        TObject* _sender;
+        std::list<callback_ptr> _callbacks;
+    public:
+        event_t(TObject* sender) : _sender(sender)
+        {
+            if(!_sender)
+                throw sender_must_be_valid_reference_error{};
+        }
+        event_t(TObject& sender) noexcept : _sender(&sender) { }
 
-		ObjectEvent(ObjectEvent const& other) noexcept :
-			_sender(other._sender), _callbacks(other._callbacks) { }
-		ObjectEvent& operator=(ObjectEvent const& other) noexcept
-		{
-			_sender = other._sender;
-			_callbacks = other._callbacks;;
-			return *this;
-		}
+        event_t(event_t const& other) noexcept : _sender(other._sender), _callbacks(other._callbacks) { }
+        event_t& operator=(event_t const& other) noexcept
+        {
+            _sender = other._sender;
+            _callbacks = other._callbacks;;
+            return *this;
+        }
 
-		ObjectEvent(ObjectEvent&& other) noexcept :
-			_sender(std::exchange(other._sender, nullptr)),
-			_callbacks(std::exchange(other._callbacks, {})) { }
-		ObjectEvent& operator=(ObjectEvent&& other) noexcept
-		{
-			_sender = std::exchange(other._sender, nullptr);
-			_callbacks = std::exchange(other._callbacks, {});
-			return *this;
-		}
+        event_t(event_t&& other) noexcept :
+            _sender(std::exchange(other._sender, nullptr)),
+            _callbacks(std::exchange(other._callbacks, {})) { }
+        event_t& operator=(event_t&& other) noexcept
+        {
+            _sender = std::exchange(other._sender, nullptr);
+            _callbacks = std::exchange(other._callbacks, {});
+            return *this;
+        }
 
-		~ObjectEvent() { _callbacks.clear(); }
+        ~event_t() { _callbacks.clear(); }
 
-                inline void operator()(TArgs ...args) const noexcept
-		{
-                    for (auto& callback : _callbacks)
-                            callback(*_sender, args...);
-                }
-                inline void operator+=(CallbackType callback) noexcept { _callbacks.push_back(callback); }
-                inline void operator-=(CallbackType callback) noexcept { _callbacks.remove(callback); }
-	};
+        void operator()(TArgs&&... args) const
+        {
+            for (callback_ptr& callback : _callbacks)
+                (*callback)(*_sender, args...);
+        }
+        callback_ptr operator+=(callback_t&& callback) noexcept {
+            return _callbacks.emplace_back(callback);
+        }
+        callback_ptr operator+=(callback_t const& callback) noexcept {
+            return _callbacks.emplace_back(callback);
+        }
+        void operator-=(callback_t&& callback) noexcept {
+            _callbacks.remove(callback);
+        }
+        void operator-=(callback_t const& callback) noexcept {
+            _callbacks.remove(callback);
+        }
+    };
 }
 
 #endif // UTILITIES_EVENTS_HPP
